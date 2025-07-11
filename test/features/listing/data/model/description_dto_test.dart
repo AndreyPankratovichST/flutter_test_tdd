574159f856c32de7fc76a5c0d909119d471a0209
pankratovich_as <pankratovich.as@gmail.com>
2025-07-11 08:43:19 +0300

    fixed tests

--- test/features/listing/data/model/description_dto_test.dart
+++ test/features/listing/data/model/description_dto_test.dart
@@ -39,9 +39,9 @@ void main() {
       final expectedJsonMap = {
         "id": 1,
         "title": "Item #1",
-        "date": "2025-01-10T00:00:00.000",
+        "published_timestamp": "2025-01-10T00:00:00.000",
         "description": "This is a description",
-        "image": "image.png"
+        "social_image": "image.png"
       };
       expect(result, expectedJsonMap);
     });
